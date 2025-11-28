8668aa9a3c779aa151825703561d20b8509e82fa
Piotr Peter Chodur <peter@opslayer.com>
2025-11-28 11:09:14 +0100

    Update schema

--- scripts/schema.ts
+++ scripts/schema.ts
@@ -11,7 +11,7 @@ const DelegateInterests = z.enum([
   'security',
 ]);
 
- const OpenGovTracks = z.enum([
+const OpenGovTracks = z.enum([
   'root',
   'whitelisted_caller',
   'general_admin',
@@ -20,53 +20,109 @@ const DelegateInterests = z.enum([
   'fast_general_admin',
 ]);
 
- const DelegateSchema = z.object({
+const xssRegex =
+  /javascript:|data:|vbscript:|on\w+\s*=|<\s*script|<\s*iframe|<\s*object|<\s*embed|<\s*link|<\s*style|expression\s*\(/i;
+
+const encodedHtmlRegex = /&#|%3c|%3e|&lt;|&gt;|&#x/i;
+
+const baseTextRegex = /^[a-zA-Z0-9\s\-_'.,|]+$/;
+
+const DelegateSchema = z.object({
   address: z
     .string()
     .regex(/^0x[a-fA-F0-9]{40}$/),
+
   name: z
     .string()
     .min(3)
     .max(100)
-    .regex(/^[a-zA-Z0-9\s\-_'.,|]+$/),
+    .regex(baseTextRegex)
+    .refine((val) => !xssRegex.test(val), {
+      message: 'Name contains potentially unsafe content',
+    })
+    .refine((val) => !encodedHtmlRegex.test(val), {
+      message: 'Name contains encoded HTML which is not allowed',
+    }),
+
   description: z
     .string()
     .min(10)
     .max(2000)
-    .regex(/^[a-zA-Z0-9\s\-_'.,|]+$/)
-    .refine((val) => !/javascript:|data:|vbscript:|on\w+\s*=/i.test(val)),
-  interests: z
-    .array(DelegateInterests)
-    .min(1),
-  tracks: z
-    .array(OpenGovTracks)
-    .min(1),
+    .regex(baseTextRegex)
+    .refine((val) => !xssRegex.test(val), {
+      message: 'Description contains potentially unsafe content',
+    })
+    .refine((val) => !encodedHtmlRegex.test(val), {
+      message: 'Description contains encoded HTML which is not allowed',
+    }),
+
+  interests: z.array(DelegateInterests).min(1),
+
+  tracks: z.array(OpenGovTracks).min(1),
+
   twitter: z
     .string()
     .regex(/^@?[a-zA-Z0-9_]+$/)
     .max(15)
+    .refine((val) => !xssRegex.test(val), {
+      message: 'Twitter handle contains potentially unsafe content',
+    })
+    .refine((val) => !encodedHtmlRegex.test(val), {
+      message: 'Twitter handle contains encoded HTML which is not allowed',
+    })
     .optional(),
+
   website: z
-    .string()
-    .regex(/^https:\/\/.+/)
+    .url({ message: 'Website must be a valid URL' })
+    .regex(/^https:\/\/[^?#]+$/, {
+      message:
+        'Website must use HTTPS and cannot contain query parameters or hash fragments',
+    })
     .min(12)
     .max(500)
+    .refine((val) => !xssRegex.test(val), {
+      message: 'Website contains potentially unsafe content',
+    })
+    .refine((val) => !encodedHtmlRegex.test(val), {
+      message: 'Website contains encoded HTML which is not allowed',
+    })
     .optional(),
+
   forum: z
     .string()
     .regex(/^[a-zA-Z0-9._-]+$/)
     .max(100)
+    .refine((val) => !xssRegex.test(val), {
+      message: 'Forum handle contains potentially unsafe content',
+    })
+    .refine((val) => !encodedHtmlRegex.test(val), {
+      message: 'Forum handle contains encoded HTML which is not allowed',
+    })
     .optional(),
+
   ensName: z
     .string()
     .regex(/^[a-zA-Z0-9.-]+\.eth$/)
     .max(100)
+    .refine((val) => !xssRegex.test(val), {
+      message: 'ENS name contains potentially unsafe content',
+    })
+    .refine((val) => !encodedHtmlRegex.test(val), {
+      message: 'ENS name contains encoded HTML which is not allowed',
+    })
     .optional(),
+
   discussionThread: z
-    .string()
+    .url({ message: 'Discussion thread must be a valid URL' })
     .startsWith('https://forum.moonbeam.network/')
     .min(32)
     .max(500)
+    .refine((val) => !xssRegex.test(val), {
+      message: 'Discussion thread contains potentially unsafe content',
+    })
+    .refine((val) => !encodedHtmlRegex.test(val), {
+      message: 'Discussion thread contains encoded HTML which is not allowed',
+    })
     .optional(),
 });
 
